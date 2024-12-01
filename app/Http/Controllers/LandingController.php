366cc7a5c761bd26a2403f494639106b7aec5efb
Habier <Habier@users.noreply.github.com>
2024-12-01 01:31:58 +0100

    Moved logic to controllers

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+class LandingController extends Controller
+{
+    public function __invoke()
+    {
+
+        if (auth()->check()) {
+            return redirect()->route('dashboard');
+        }
+
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,20 @@
 <?php
 
 use App\Http\Controllers\SocialLoginController;
-use GuzzleHttp\RequestOptions;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use Laravel\Socialite\Facades\Socialite;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-})->name('home');
+Route::get(\App\Http\Controllers\LandingController::class)->name('home');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', \App\Http\Controllers\DashBoardController::class)->name('dashboard');
 });
 
 
-Route::get('/auth/redirect', [SocialLoginController::class, 'redirect']);
+Route::get('/auth/redirect', [SocialLoginController::class, 'redirect'])->name('login');
 
 Route::get('/auth/callback', [SocialLoginController::class, 'callback']);
 
