d0c00b3d3e19dd0630e0d1bada4dd1f3c93c5915
Habier <Habier@users.noreply.github.com>
2024-12-07 00:56:48 +0100

    Added Retort factories and models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
+        if (app()->environment() == 'local') {
+            $this->call([
+                DevSeeder::class,
+            ]);
+        }
     }
 }
